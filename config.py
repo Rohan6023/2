067d0ede7992a3a4efb2846a5eb96aaa681ba753
Rohan singh <151426524+Rohan6023@users.noreply.github.com>
2024-10-07 19:39:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7221614994:AAEa_YmyyyG0ghKeclLtykRnIA8CmsjH8_M")
+BOT_TOKEN=("7994038330:AAGxa3u_AIW0s3655lDARfMLGArHlzmA3JI")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("ninjagamerop")
